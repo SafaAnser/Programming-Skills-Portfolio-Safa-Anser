8697d75b8cef77a98ec63cf542595f7567a551b7
SafaAnser <safaanser.rak@gmail.com>
2024-11-23 23:36:10 +0400

    Create Exercise 03- Biography.py

---
+++ Exercise 03- Biography.py
@@ -0,0 +1,11 @@
+#asking user to input first and second name
+name=input('enter your first and second name: ')
+
+#asking user to input hometown
+hometown=input('enter your hometown: ')
+
+#asking user to input age
+age=(input('enter your age: '))
+
+#printing the value in seperate line using single print statement
+print(f'name:{name}\nhometown:{hometown}\nage:{age}')
